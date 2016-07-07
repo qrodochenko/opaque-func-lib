5efc15c00a2be07f9103158a342caff54294bab0
Àíãåëèíà <asenchukova@yandex.ru>
2016-07-07 15:56:58 +0300

    Senchukova first commit

---
+++ Senchukova/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunctionsLibrary;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double k = Cinterval_ww_finfin_1.interval_ww_finfin_1(3);
+
+            Console.WriteLine(k);
+            Console.WriteLine(Cinterval_ww_finfin_1_in.interval_ww_finfin_1_in());
+            Console.ReadKey();
+            
+        }
+    }
+}
