c3555ff14186868e5554e5c58f38746ac4963314
kirill-vedenev <vedenevk@yandex.ru>
2016-07-07 14:46:43 +0300

    Vedenev+FirstCommit

---
+++ Vedenev/SummerPractice_2016/ModuleTests/OpaqueFunctionsUnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpaqueFunctions;
+
+namespace ModuleTests
+{
+    [TestClass]
+    public class OpaqueFunctionsUnitTest1
+    {
+        [TestMethod]
+        public void Opaque1SinCosTest1()
+        {
+            double X = Opaque1SinCos.Body(360, 10);
+            Assert.IsTrue(Math.Abs(X - 1) < double.Epsilon, "Значение функции не единица!");
+        }
+    }
+
+
+    [TestClass]
+    public class Ctest_L00_58_1_arctg_arcctg
+    {
+        double persicion = 0.000000000000001;
+        [TestMethod]
+        public void Opaque1SinCosTest1()
+        {
+            double mx; 
+            double X1 = CL00_58_1_arctg_arcctg.Body(1);
+            mx = X1;
+            double X2 = CL00_58_1_arctg_arcctg.Body(0.53);
+            if (X2 > mx) mx = X2; 
+            double X3 = CL00_58_1_arctg_arcctg.Body(0.0001);
+            if (X3 > mx) mx = X3; 
+            double X4 = CL00_58_1_arctg_arcctg.Body(360);
+            if (X4 > mx) mx = X4; 
+            double X5 = CL00_58_1_arctg_arcctg.Body(1547.547921);
+            if (X5 > mx) mx = X5; 
+            double X6 = CL00_58_1_arctg_arcctg.Body(12);
+            if (X6 > mx) mx = X6; 
+            double X7 = CL00_58_1_arctg_arcctg.Body(6641);
+            if (X7 > mx) mx = X7; 
+            double X8 = CL00_58_1_arctg_arcctg.Body(0.00000614);
+            if (X8 > mx) mx = X8; 
+            double X9 = CL00_58_1_arctg_arcctg.Body(897);
+            if (X9 > mx) mx = X9; 
+            double X10 = CL00_58_1_arctg_arcctg.Body(92);
+            if (X10 > mx) mx = X10; 
+
+            double mid = (X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10) / 10;
+            Assert.IsTrue(Math.Abs(mx) < persicion, "Err");
+        }
+    }
+}
+
+
---
+++ Vedenev/SummerPractice_2016/OpaqueFunctions/OpaqueFunctions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace OpaqueFunctions
+{
+    /// <summary>
+    /// Реализует основное тригонометрическое тождество sin^2 (x) + cos^2 (x) = 1,  
+    /// где угол X задается параметром в радианах <paramref name="angle"/>. 
+    /// Результатом функции является целое число X - результат умножения левой части тождества само на себя столько раз,
+    /// сколько задано параметром <paramref name="count"/>.
+    /// </summary>
+    /// <param name="angle">Угол в радианах</param>
+    /// <param name="count">Количество требуемых перемножений</param>
+    /// <returns>1</returns>
+    [OpaqueFunction()]
+    [FunctionName("Opaque1", "sin^2 + cos^2 = 1")]
+    [EquivalentIntConstant(1)]
+    public static class Opaque1SinCos
+    {            
+        public static double Body(double angle, int count)
+        {
+            double X = 1;
+            for (int i = 0; i < count; i++)
+            {
+                X *= Math.Sin(angle)*Math.Sin(angle) + Math.Cos(angle)*Math.Cos(angle);
+            }
+            return X;
+        }
+    }
+
+    
+    //##################################################################################################
+    /// <summary>
+    /// Реализует тригонометрическое тождество arctg(x)-arcctg(1/x) = 0,  
+    /// где X некоторое положительное число <paramref name="x"/>. 
+    /// Результатом функции является целое число X - результат умножения левой части тождества само на себя столько раз,
+    /// сколько задано параметр.
+    /// </summary>
+    /// <param name="x">Положительное число</param>
+    /// <returns>1</returns>
+    [OpaqueFunction()]
+    [FunctionName("L00_58_1_arctg_arcctg", "arctg(x)-arcctg(1/x) = 0")]
+    [EquivalentIntConstant(0)]
+    public static class CL00_58_1_arctg_arcctg
+    {
+        public static double Body(double x)
+        {
+            
+            double y1 = Math.Atan(x);
+            double y2 = Math.PI/2 - Math.Atan(1/x);
+            return y1 - y2;
+        }
+    }
+
+    [OpaqueFunction()]
+    [FunctionName("L00_58_1_arctg_arcctg_in", "(0, w)")]
+    public static class CL00_58_1_arctg_arcctg_in
+    {
+        public static string Body()
+        {
+            return "(0, w)";
+        }
+    }
+
+
+    //##################################################################################################
+    /// <summary>
+    /// Реализует тригонометрическое тождество arctg(x) – arccos(1/((1+x*x)^1/2)) = 0,  
+    /// где X некоторое положительное число <paramref name="x"/>. 
+    /// Результатом функции является целое число X - результат умножения левой части тождества само на себя столько раз,
+    /// сколько задано параметр.
+    /// </summary>
+    /// <param name="x">Положительное число</param>
+    /// <returns>1</returns>
+    [OpaqueFunction()]
+    [FunctionName("L00_59_1_arctg_arccos", "arctg(x) – arccos(1/((1+x*x)^1/2)) = 0")]
+    [EquivalentIntConstant(0)]
+    public static class CL00_59_1_arctg_arccos
+    {
+        public static double Body(double x)
+        {
+
+            double y1 = Math.Atan(x);
+            double y2 = Math.Acos(1 /Math.Sqrt((1 + x * x)));
+            return y1 - y2;
+        }
+    }
+
+    [OpaqueFunction()]
+    [FunctionName("L00_59_1_arctg_arccos_in", "(0, w)")]
+    public static class CL00_58_1_arctg_arcctg_in
+    {
+        public static string Body()
+        {
+            return "(0, w)";
+        }
+    }
+
+}
+
