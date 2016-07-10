fd9512dfa46e1e2a1b15b8890b1026a50ae415b7
Bakumenko <bakumenkoleg@gmail.com>
2016-07-11 00:15:41 +0300

    add test for me

--- Bakumenko/ModuleTests/OpaqueFunctionsUnitTest1.cs
+++ Bakumenko/ModuleTests/OpaqueFunctionsUnitTest1.cs
@@ -4,4 +4,15 @@ using OpaqueFunctions;
 
 namespace ModuleTests
 {
+    [TestClass]
+    public class OpaqueFunctionsUnitTest1
+    {
+        [TestMethod]
+        public void Opaque1SinCosTest1()
+        {
+            double X = Opaque1SinCos.Body(360, 10);
+            Assert.IsTrue(Math.Abs(X - 1) < double.Epsilon, "Значение функции не единица!");
+        }
+    }
+
 }
--- Bakumenko/OpaqueFunctions/OpaqueFunctions.cs
+++ Bakumenko/OpaqueFunctions/OpaqueFunctions.cs
@@ -3,6 +3,31 @@
 namespace OpaqueFunctions
 {
     /// <summary>
+    /// Реализует основное тригонометрическое тождество sin^2 (x) + cos^2 (x) = 1,  
+    /// где угол X задается параметром в радианах <paramref name="angle"/>. 
+    /// Результатом функции является целое число X - результат умножения левой части тождества само на себя столько раз,
+    /// сколько задано параметром <paramref name="count"/>.
+    /// </summary>
+    /// <param name="angle">Угол в радианах</param>
+    /// <param name="count">Количество требуемых перемножений</param>
+    /// <returns>1</returns>
+    [OpaqueFunction()]
+    [FunctionName("Opaque1", "sin^2 + cos^2 = 1")]
+    [EquivalentIntConstant(1)]
+    public static class Opaque1SinCos
+    {
+        public static double Body(double angle, int count)
+        {
+            double X = 1;
+            for (int i = 0; i < count; i++)
+            {
+                X *= Math.Sin(angle) * Math.Sin(angle) + Math.Cos(angle) * Math.Cos(angle);
+            }
+            return X;
+        }
+    }
+
+    /// <summary>
     /// Реализует 30.	f(x) = sin(arccos(x)) –(1 – x*x)^1/2 
     /// </summary>
     /// <param name="angle">Угол в радианах</param>
