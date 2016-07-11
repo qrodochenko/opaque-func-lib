758b3863f26e399ede1190709cfaaec0f4d9408e
IvanMarkov <vanymarkov@rambler.ru>
2016-07-11 15:46:50 +0300

    Не работает секанс, нет комментариев, нет тестов

---
+++ Markov/ModuleTests/OpaqueFunctionsUnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpaqueFunctions;
+
+namespace ModuleTests
+{
+    [TestClass]
+    public class OpaqueFunctionsUnitTest1
+    {
+        [TestMethod]
+        public void Opaque1SinCosTest1()
+        {
+            double X = Opaque1SinCos.Body(360, 10);
+            Assert.IsTrue(Math.Abs(X - 1) < double.Epsilon, "Значение функции не единица!");
+            
+        }
+
+        
+    }
+}
