46da811a6b5c2ae143c115299b95c8809040c4a6
Bakumenko <bakumenkoleg@gmail.com>
2016-07-12 04:52:41 +0300

    prepare for Test

--- Bakumenko/ModuleTests/OpaqueFunctionsUnitTest1.cs
+++ Bakumenko/ModuleTests/OpaqueFunctionsUnitTest1.cs
@@ -4,15 +4,29 @@ using OpaqueFunctions;
 
 namespace ModuleTests
 {
-    [TestClass]
-    public class OpaqueFunctionsUnitTest1
-    {
-        [TestMethod]
-        public void Opaque1SinCosTest1()
-        {
-            double X = Opaque1SinCos.Body(360, 10);
-            Assert.IsTrue(Math.Abs(X - 1) < double.Epsilon, "Значение функции не единица!");
+    [TestClass] 
+    public class CTest_L00_30_1_sin_arccos
+    { 
+        [TestMethod] 
+        public void Test_L00_30_1_sin_arccos()
+        { 
+            //выбрал собственный порог ошибки 
+            double treshold = 1e-14; 
+            var rnd = new Random(); 
+            var arg = rnd.NextDouble() * double.MaxValue;
+
+            double F = CL00_30_1_sin_arccos.L00_30_1_sin_arccos(arg);
+            Assert.IsTrue(Math.Abs(F - 0) < treshold, "Значение функции не ноль!");
+            //Assert.IsTrue(Math.Abs(F - 1) < double.Epsilon, "Значение функции не единица!");
         }
-    }
+
+        [TestMethod] 
+        public void Test_L00_103_3_log_in()
+        { 
+            string str = "(0, w)";
+            string F = CL00_30_1_sin_arccos_in.L00_30_1_sin_arccos_in();
+            Assert.IsTrue(F == str, "Область определения задана неверно!"); 
+        } 
+    } 
 
 }
