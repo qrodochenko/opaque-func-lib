812a99f6f3a26248a6c83b7b3d3b1b235659f884
nick434434 <kovalnick434@gmail.com>
2016-07-14 14:57:14 +0300

    Сделал тесты для всех функций.

--- Kovalev/ModuleTests/OpaqueFunctionsUnitTest1.cs
+++ Kovalev/ModuleTests/OpaqueFunctionsUnitTest1.cs
@@ -40,4 +40,58 @@ namespace ModuleTests
             }
         }
     }
+
+    [TestClass]
+    public class TMathApprox_3
+    {
+        [TestMethod]
+        public void MathApprox_3_Compute_2_Test()
+        {
+            Random rnd = new Random();
+            double error = 0.000000005;
+            for (int i = 0; i < 1000; i++)
+            {
+                double arg = rnd.Next(1, 2000) - 1000;
+                arg = arg / 2000.0;
+                Assert.IsTrue((Math.Abs(CMathApprox_3_Compute.MathApprox_3_Compute_2(arg, error)
+                    - Math.Asin(arg)) < error), "Значение недостаточно близко к реальному");
+            }
+        }
+    }
+
+    [TestClass]
+    public class TMathApprox_4
+    {
+        [TestMethod]
+        public void MathApprox_4_Compute_2_Test()
+        {
+            Random rnd = new Random();
+            double error = 0.000000005;
+            for (int i = 0; i < 1000; i++)
+            {
+                double arg = rnd.Next(1, 2000) - 1000;
+                arg = arg / 2000.0;
+                Assert.IsTrue((Math.Abs(CMathApprox_4_Compute.MathApprox_4_Compute_2(arg, error)
+                    -Math.PI / 2 + Math.Atan(arg)) < error), "Значение недостаточно близко к реальному");
+            }
+        }
+    }
+
+    [TestClass]
+    public class TMathApprox_5
+    {
+        [TestMethod]
+        public void MathApprox_5_Compute_2_Test()
+        {
+            Random rnd = new Random();
+            double error = 0.000000005;
+            for (int i = 0; i < 1000; i++)
+            {
+                double arg = rnd.Next(1, 2000) - 1000;
+                arg = arg / 2000.0;
+                Assert.IsTrue((Math.Abs(CMathApprox_5_Compute.MathApprox_5_Compute_2(arg, error)
+                    - Math.Acos(arg)) < error), "Значение недостаточно близко к реальному");
+            }
+        }
+    }
 }
